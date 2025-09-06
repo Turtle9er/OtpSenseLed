4002479043a081b64f24b7fdb4143757190465ee
Turtle9er <mattpjensen@gmail.com>
2025-09-05 21:19:20 -0700

    removed digitalfast and added bLed

--- Led.cpp
+++ Led.cpp
@@ -13,6 +13,7 @@ LedControl Led;
 LedControl rLed;
 LedControl gLed;
 LedControl yLed;
+LedControl bLed
 
 void LedControl::begin(int ledPin)
 {
@@ -23,7 +24,7 @@ void LedControl::begin(int ledPin)
 void LedControl::beat(uint32_t rate1, uint32_t rate2)
 {
 	static uint32_t prev = millis();
-	uint8_t state = digitalReadFast(_ledPin);
+	uint8_t state = digitalRead(_ledPin);
 #ifdef INVERSE_SIGNAL
 	uint32_t pulse = state ? rate1 : rate2;
 #else
@@ -31,23 +32,23 @@ void LedControl::beat(uint32_t rate1, uint32_t rate2)
 #endif
 	if (millis() - prev > pulse)
 	{
-		digitalWriteFast(_ledPin, !state);
+		digitalWrite(_ledPin, !state);
 		prev = millis();
 	}
 }
 
 void LedControl::swap()
 {
-	uint8_t state = digitalReadFast(_ledPin);
-	digitalWriteFast(_ledPin, !state);
+	uint8_t state = digitalRead(_ledPin);
+	digitalWrite(_ledPin, !state);
 }
 
 void LedControl::off()
 {
-	digitalWriteFast(_ledPin, _OFF);
+	digitalWrite(_ledPin, _OFF);
 }
 
 void LedControl::on()
 {
-	digitalWriteFast(_ledPin, _ON);
+	digitalWrite(_ledPin, _ON);
 }
